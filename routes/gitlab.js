b1c5ab04db188d5b587d0c56e81a657f89ddfb10
ikethecoder <ikethecoder@canzea.com>
2019-04-24 15:48:44 -0700

    Update gitlab.js

--- routes/gitlab.js
+++ routes/gitlab.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/webhook', function (req, res) {
-  console.log(req);
   if (req.body.event_name === 'user_create') {
-    gitlabLdapGroupSync.sync();
+    setTimeout(() => {
+      gitlabLdapGroupSync.sync();
+    }, 5000);
     res.status(200).send('OK');
   } else {
     res.status(422).send('This is not a valid gitlab system hook');
