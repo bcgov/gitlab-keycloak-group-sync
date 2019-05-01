519564fa83b4600c0e7da65bdb84fcca243652e5
Ike The Coder <ikethecoder@canzea.com>
2019-05-01 12:24:03 -0700

    change from using ldap to keycloak as source of group membership

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function (req, res) {
-  res.send('Gitlab LDAP User Sync');
+  res.send('Gitlab Group User Sync');
 });
 
 module.exports = router;
